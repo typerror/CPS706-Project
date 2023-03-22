5e9f57b21b312b6cd7e8b05a9925bcbde7fc7aba
MateenOmar <omarmateen129@gmail.com>
2023-03-22 18:22:27 -0400

    Adding dijsktra's algo

---
+++ centralizedGraph.py
@@ -0,0 +1,66 @@
+class CentralizedGraph:
+    def __init__(self, nodes):
+        self.nodes = nodes
+        self.edges = [[0 for _ in range(nodes)] for _ in range(nodes)]
+        self.cost = [9999999] * nodes
+        self.visited = [False] * nodes
+
+    def addEdge(self, source, destination, cost):
+        self.edges[source][destination] = cost
+
+    def getEdge(self, source, destination):
+        return self.edges[source][destination]
+
+    def getCost(self, node):
+        return self.cost[node]
+
+    def getVisited(self, node):
+        return self.visited[node]
+
+    def minCost(self):
+        minIndex = 0
+        min = 9999999
+
+        for i in range(self.nodes):
+            if self.cost[i] < min and self.visited[i] == False:
+                min = self.cost[i]
+                minIndex = i
+
+        return minIndex
+
+    def minPathFindIterative(self, source):
+        self.cost[source] = 0
+
+        for _ in range(self.nodes):
+            min = self.minCost()
+            self.visited[min] = True
+
+            for j in range(self.nodes):
+                if self.edges[min][j] != 0 and self.visited[j] == False and self.cost[j] > self.cost[min] + self.edges[min][j]:
+                    self.cost[j] = self.cost[min] + self.edges[min][j]
+            break
+
+        self.printCost()
+
+    def minPathFind(self, source):
+        self.cost[source] = 0
+
+        for _ in range(self.nodes):
+            min = self.minCost()
+            self.visited[min] = True
+
+            for j in range(self.nodes):
+                if self.edges[min][j] != 0 and self.visited[j] == False and self.cost[j] > self.cost[min] + self.edges[min][j]:
+                    self.cost[j] = self.cost[min] + self.edges[min][j]
+
+        self.printCost()
+
+    def printEdges(self):
+        for i in range(self.nodes):
+            print(self.edges[i])
+        print()
+
+    def printCost(self):
+        for node in range(self.nodes):
+            print(node, "\t", self.cost[node])
+        print()
---
+++ main.py
@@ -0,0 +1,20 @@
+from centralizedGraph import CentralizedGraph
+
+graph = CentralizedGraph(5)
+graph.addEdge(0, 1, 10)
+graph.addEdge(0, 2, 5)
+graph.addEdge(1, 3, 1)
+graph.addEdge(1, 2, 2)
+graph.addEdge(2, 1, 3)
+graph.addEdge(2, 3, 9)
+graph.addEdge(2, 4, 2)
+graph.addEdge(3, 4, 4)
+graph.addEdge(4, 3, 6)
+graph.addEdge(4, 0, 7)
+graph.printEdges()
+# graph.minPathFind(0)
+
+# graph.minPathFindIterative(0)
+# graph.minPathFindIterative(0)
+# graph.minPathFindIterative(0)
+# graph.minPathFindIterative(0)
